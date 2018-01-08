e85a37e0701c3aaa00b9e4dbffaebdf014ba72f9
Duong Dang Hoang <alex@Duongs-iMac.local>
2018-01-08 17:31:50 +1100

    Initialize

---
+++ coderSwag/coderSwag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coderSwag
+//
+//  Created by Duong Dang Hoang on 8/1/18.
+//  Copyright © 2018 Duong Dang Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
